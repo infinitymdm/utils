c248d56d5386f06c9b7951e532593f5a16b7dec2
Marcus Mellor <infinitymdm@gmail.com>
2024-06-13 07:59:14 -0500

    gen_pads: add PADINOUT

--- sv/gen_pads.py
+++ sv/gen_pads.py
@@ -8,24 +8,26 @@ def parse_line(line: str):
 
         Returns a tuple of the following form:
             (  
-                bool: True if this is an output,
+                str:  Port direction ('inout', 'output', or 'input'),
                 int:  Number of wires,
                 str:  Name of the bus/wire
             )
-        For example, the line "input logic [31:0] bus;" should get parsed to (False, 32, "bus")
+        For example, the line "input logic [31:0] bus;" should get parsed to ('input', 32, "bus")
     '''
 
     # Initial assumptions
-    is_output = False
+    direction = 'inout'
     wire_count = 1
-    bus_name = "none"
+    bus_name = 'none'
 
     tokens = line.strip().split()
     for token in tokens:
         if any(typestr == token.strip() for typestr in ['reg', 'wire', 'integer', 'real', 'time', 'realtime', 'logic', 'bit', 'byte', 'shortint', 'int', 'longint', 'shortreal']):
             continue # skip types
         elif 'output' in token:
-            is_output = True
+            direction = 'output'
+        elif 'input' in token:
+            direction = 'input'
         elif '[' in token:
             # Assume we have the whole [n:m] bit in this token
             n, m = token.strip()[1:-1].split(':')
@@ -40,25 +42,29 @@ def parse_line(line: str):
         else:
             # If we still don't have a match, it's probably the name
             bus_name = ''.join([c for c in token if c.isalnum() or c == '_'])
-    return (is_output, wire_count, bus_name)
+    return (direction, wire_count, bus_name)
 
-def gen_input_pad(pin_num: int, wire_name: str, is_bus=False, bus_addr=0):
+def gen_inout_pad(pin_num: int, wire_name: str, is_bus=False, bus_addr=0, has_ien=False):
+    ''' Generate an inout pad string'''
+    _ = "'"
+    bus_str = "["+str(bus_addr)+"]" if is_bus else ""
+    return f'PADINOUT p{pin_num:03}(.PAD({wire_name}{bus_str}), .OEN({wire_name}_oen_o{bus_str}), .DO({wire_name}_o{bus_str}), .IEN({f"{wire_name}_ien_o{bus_str}" if has_ien else f"1{_}b1"}), .DI({wire_name}_i{bus_str}));'
+
+def gen_input_pad(pin_num: int, wire_name: str, is_bus=False, bus_addr=0, has_ien=False):
     ''' Generate an input pad string
 
-        If the wire is part of a bus, specify the address with bus (e.g. bus=12 for addr[12])
+        If the wire is part of a bus, specify the address with is_bus and bus_addr (e.g. is_bus=True, bus_addr=12 for addr[12])
     '''
-    if '_in' in wire_name:
-        wire_name = wire_name.replace('_in', '')
-    return f'PADIN  p{pin_num:03}(.DI({wire_name}_i{"["+str(bus_addr)+"]" if is_bus else ""}), .PAD({wire_name}{"["+str(bus_addr)+"]" if is_bus else ""}));'
+    bus_str = "["+str(bus_addr)+"]" if is_bus else ""
+    return f'PADIN    p{pin_num:03}(.PAD({wire_name}{bus_str}), .DI({wire_name}_i{bus_str}));'
 
-def gen_output_pad(pin_num: int, wire_name: str, is_bus=False, bus_addr=0):
+def gen_output_pad(pin_num: int, wire_name: str, is_bus=False, bus_addr=0, has_ien=False):
     ''' Generate an output pad string
 
-        If the wire is part of a bus, specify the address with bus (e.g. bus=12 for addr[12])
+        If the wire is part of a bus, specify the address with is_bus and bus_addr (e.g. is_bus=True, bus_addr=12 for addr[12])
     '''
-    if '_out' in wire_name:
-        wire_name = wire_name.replace('_out', '')
-    return f'PADOUT p{pin_num:03}(.DO({wire_name}_o{"["+str(bus_addr)+"]" if is_bus else ""}), .PAD({wire_name}{"["+str(bus_addr)+"]" if is_bus else ""}));'
+    bus_str = "["+str(bus_addr)+"]" if is_bus else ""
+    return f'PADOUT   p{pin_num:03}(.PAD({wire_name}{bus_str}), .DO({wire_name}_o{bus_str}));'
 
 def gen_pads(io_lines: list):
     ''' Given a list of verilog io lines, generates a list of pad strings.'''
@@ -66,59 +72,28 @@ def gen_pads(io_lines: list):
     for line in io_lines:
         if not len(line.strip()):
             continue # Skip empty lines
-        (is_output, wire_count, name) = parse_line(line)
-        pad_generator = gen_output_pad if is_output else gen_input_pad
+        (direction, wire_count, name) = parse_line(line)
+        pad_generator = gen_inout_pad
+        has_ien=False
+        if direction == 'output':
+            pad_generator = gen_output_pad
+        elif direction == 'input':
+            pad_generator = gen_input_pad
+        else:
+            has_ien = input(f'Does {name} have an input enable [y/N]? ').lower().startswith('y')
         if wire_count == 1:
-            pad_lines.append(pad_generator(len(pad_lines), name))
+            pad_lines.append(pad_generator(len(pad_lines), name, has_ien=has_ien))
         else:
             for i in range(wire_count):
-                pad_lines.append(pad_generator(len(pad_lines), name, True, i))
+                pad_lines.append(pad_generator(len(pad_lines), name, is_bus=True, bus_addr=i, has_ien=has_ien))
     return pad_lines
 
 if __name__ == '__main__':
-    io_lines = """
-        input  logic                 clk;
-        input  logic                 reset_ext;
-        output logic                 reset;
-        input  logic                 TIMECLK;
-        input  logic [31:0]          GPIOPinsIn;
-        output logic [31:0]          GPIOPinsOut;
-        output logic [31:0]          GPIOPinsEn;
-        input  logic                 UARTSin;
-        output logic                 UARTSout;
-        input  logic                 SDCIntr;
-        input  logic                 SPIIn;
-        output logic                 SPIOut;
-        output logic [3:0]           SPICS;
-        output logic                 ddr_ck_p;
-        output logic                 ddr_ck_n;
-        output logic                 ddr_cke;
-        output logic [2:0]           ddr_ba;
-        output logic [15:0]          ddr_addr;
-        output logic                 ddr_cs;
-        output logic                 ddr_ras;
-        output logic                 ddr_cas;
-        output logic                 ddr_we;
-        output logic                 ddr_reset;
-        output logic                 ddr_odt;
-        output logic [P.XLEN/16-1:0] ddr_dm_oen;
-        output logic [P.XLEN/16-1:0] ddr_dm;
-        output logic [P.XLEN/16-1:0] ddr_dqs_p_oen;
-        output logic [P.XLEN/16-1:0] ddr_dqs_p_ien;
-        output logic [P.XLEN/16-1:0] ddr_dqs_p_out;
-        input  logic [P.XLEN/16-1:0] ddr_dqs_p_in;
-        output logic [P.XLEN/16-1:0] ddr_dqs_n_oen;
-        output logic [P.XLEN/16-1:0] ddr_dqs_n_ien;
-        output logic [P.XLEN/16-1:0] ddr_dqs_n_out;
-        input  logic [P.XLEN/16-1:0] ddr_dqs_n_in;
-        output logic [P.XLEN/2-1:0]  ddr_dq_oen;
-        output logic [P.XLEN/2-1:0]  ddr_dq_out;
-        input  logic [P.XLEN/2-1:0]  ddr_dq_in;
-        input  logic                 dfi_clk_2x;
-        output logic                 dfi_clk_1x;
-        """
+    io_lines = ''
+    with open('io.txt', 'r', encoding='utf-8') as file:
+        io_lines = file.read()
     pad_lines = gen_pads(io_lines.split('\n'))
-    with open('pads.sv', 'w', encoding='utf-8') as file:
+    with open('pads.txt', 'w', encoding='utf-8') as file:
         for line in pad_lines:
             file.write(line + '\n')
         file.close()
